fcc41168de384daf65a3846348d8e4a51e1ffeab
Filippo Fadda <filippo.fadda@gmail.com>
2012-10-10 17:06:00 +0200

    same

---
+++ src/ElephantOnCouch/Handlers/FilterHandler.php
@@ -0,0 +1,14 @@
+<?php
+
+//! @file FilterHandler.class.php
+//! @brief This file contains the FiltersHandler class.
+//! @details
+//! @author Filippo F. Fadda
+
+namespace ElephantOnCouch;
+
+
+//! @brief TODO
+class FiltersHandler {
+
+}
