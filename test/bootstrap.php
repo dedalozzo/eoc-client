bedb67ee52deb083b2286a26d038bc97bc524843
Filippo Fadda <filippo.fadda@gmail.com>
2012-11-07 05:12:42 +0100

    just added

---
+++ test/bootstrap.php
@@ -0,0 +1,20 @@
+<?php
+
+//! @file bootstrap.php
+//! @brief This is the bootstrap file for tests.
+//! @details
+//! @author Filippo F. Fadda
+
+
+use ElephantOnCouch\ElephantOnCouch;
+//use ElephantOnCouch\ResponseException;
+//use ElephantOnCouch\Docs\Doc;
+
+
+const USE_CURL = TRUE; // To use socket for tests comment this.
+
+const COUCH_DATABASE = "blog";
+const COUCH_USER = "pippo";
+const COUCH_PASSWORD = "calippo";
+
+?>
