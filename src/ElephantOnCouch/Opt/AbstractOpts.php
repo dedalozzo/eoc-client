e4f4539ae7f64de734b7c72dbad0c26c4719243e
Filippo Fadda <filippo.fadda@gmail.com>
2013-06-17 18:04:46 +0200

    just added

---
+++ src/ElephantOnCouch/Opt/AbstractOpts.php
@@ -0,0 +1,34 @@
+<?php
+
+//! @file AbstractOpts.php
+//! @brief This file contains the AbstractOpts class.
+//! @details
+//! @author Filippo F. Fadda
+
+
+//! @brief The CouchDB's options namespace.
+namespace ElephantOnCouch\Opt;
+
+
+//! @brief Superclass of all options classes.
+//! @nosubgrouping
+abstract class AbstractOpts {
+
+  protected $options = [];
+
+
+  //! @brief Resets the options.
+  public function reset() {
+    unset($this->options);
+    $this->options = [];
+  }
+
+
+  //! @brief Returns an associative array of the chosen options.
+  //! @details Used internally by ElephantOnCouch methods.
+  //! @return associative array
+  public function asArray() {
+    return $this->options;
+  }
+
+}
