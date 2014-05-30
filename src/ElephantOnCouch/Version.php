35ae7b0ee1893eb7890fdeecb62921881f5b5690
Filippo Fadda <filippo.fadda@gmail.com>
2014-05-30 13:35:20 +0200

    added Version class

---
+++ src/ElephantOnCouch/Version.php
@@ -0,0 +1,34 @@
+<?php
+
+/**
+ * @file Version.php
+ * @brief This file contains the Version class.
+ * @details
+ * @author Filippo F. Fadda
+ */
+
+
+namespace ElephantOnCouch;
+
+
+/**
+ * @brief This helper class is aimed to provide the software version number.
+ * @details The version number is composed by a group of three numbers. The first one is the major release number, the
+ * second one is the minor release number and the third one is used to identify the maintenance version.
+ */
+class Version {
+
+  const MAJOR = '0'; //!< Major release number.
+  const MINOR = '3'; //!< Minor release number.
+  const MAINTENANCE = '0'; //!< Maintenance release number (bug fixes only).
+
+
+  /**
+   * @brief Returns the version number as string.
+   * @return string The version number.
+   */
+  public static function getNumber() {
+    return "{${self::MAJOR}}.{${self::MINOR}}.{${self::MAINTENANCE}}";
+  }
+
+} 
