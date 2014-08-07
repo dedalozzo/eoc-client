7dfde6b16c35be1a0e76c7d11ad6d5cc4ab36c2a
Filippo Fadda <filippo.fadda@gmail.com>
2014-08-07 20:32:11 +0200

    added the unsetOpt() method

--- src/ElephantOnCouch/Opt/ViewQueryOpts.php
+++ src/ElephantOnCouch/Opt/ViewQueryOpts.php
@@ -30,9 +30,25 @@ class ViewQueryOpts extends AbstractOpts {
 
 
   /**
+   * @brief Removes an option previously set.
+   * @param[in] string $name The option name.
+   * @return ViewQueryOpts
+   * @attention Chainable.
+   */
+  public function unsetOpt($name) {
+    if (array_key_exists($name, $this->options))
+      unset($this->options['name']);
+
+    return $this;
+  }
+
+
+  /**
    * @brief Returns only documents that match the specified key.
    * @param[in] string $value The key.
    * @param[in] bool $encode (optional) JSON encodes `$value`.
+   * @return ViewQueryOpts
+   * @attention Chainable.
    */
   public function setKey($value, $encode = TRUE) {
     $this->options["key"] = $encode ? json_encode($value) : $value;
@@ -50,6 +66,8 @@ class ViewQueryOpts extends AbstractOpts {
    * @brief Defines the first key to be included in the range.
    * @param[in] string $value The key at which to start the range.
    * @param[in] bool $encode (optional) JSON encodes `$value`.
+   * @return ViewQueryOpts
+   * @attention Chainable.
    */
   public function setStartKey($value, $encode = TRUE) {
     $this->options["startkey"] = $encode ? json_encode($value) : $value;
@@ -61,6 +79,8 @@ class ViewQueryOpts extends AbstractOpts {
    * @brief Defines the last key to be included in the range.
    * @param[in] string $value The key at which to end the range.
    * @param[in] bool $encode (optional) JSON encodes `$value`.
+   * @return ViewQueryOpts
+   * @attention Chainable.
    */
   public function setEndKey($value, $encode = TRUE) {
     $this->options["endkey"] = $encode ? json_encode($value) : $value;
@@ -86,6 +106,8 @@ class ViewQueryOpts extends AbstractOpts {
   /**
    * @brief Sets the ID of the document with which to start the range.
    * @param[in] string $value The ID of the document with which to start the range.
+   * @return ViewQueryOpts
+   * @attention Chainable.
    */
   public function setStartDocId($value) {
     $this->options["startkey_docid"] = $value;
@@ -96,6 +118,8 @@ class ViewQueryOpts extends AbstractOpts {
   /**
    * @brief Sets the ID of the document with which to end the range.
    * @param[in] string $value The ID of the document with which to end the range.
+   * @return ViewQueryOpts
+   * @attention Chainable.
    */
   public function setEndDocId($value) {
     $this->options["endkey_docid"] = $value;
@@ -108,7 +132,9 @@ class ViewQueryOpts extends AbstractOpts {
   /**
    * @brief Restricts the number of results.
    * @details Allowed values: positive integers.
-   * @param[in] integer $value The maximum number of rows to include in the output.
+   * @param[in] int $value The maximum number of rows to include in the output.
+   * @return ViewQueryOpts
+   * @attention Chainable.
    */
   public function setLimit($value) {
     if (is_int($value) && $value > 0)
@@ -125,6 +151,8 @@ class ViewQueryOpts extends AbstractOpts {
    * @details The group option controls whether the reduce function reduces to a set of distinct keys or to a single
    * result row. This will run the rereduce procedure. This parameter makes sense only if a reduce function is defined
    * for the view.
+   * @return ViewQueryOpts
+   * @attention Chainable.
    */
   public function groupResults() {
     $this->options["group"] = "true";
@@ -137,7 +165,9 @@ class ViewQueryOpts extends AbstractOpts {
    * @details If your keys are JSON arrays, this parameter will specify how many elements in those arrays to use for
    * grouping purposes. If your emitted keys are not JSON arrays this parameter's value will effectively be ignored.
    * Allowed values: positive integers.
-   * @param[in] integer $value The number of elements used for grouping purposes.
+   * @param[in] int $value The number of elements used for grouping purposes.
+   * @return ViewQueryOpts
+   * @attention Chainable.
    */
   public function setGroupLevel($value) {
     if (is_int($value) && $value > 0) {
@@ -153,6 +183,8 @@ class ViewQueryOpts extends AbstractOpts {
 
   /**
    * @brief Calls the reduce function is defined.
+   * @return ViewQueryOpts
+   * @attention Chainable.
    */
   public function reduce() {
     $this->options["reduce"] = "true";
@@ -164,6 +196,8 @@ class ViewQueryOpts extends AbstractOpts {
    * @brief Even if a reduce function is defined for the view, doesn't call it.
    * @details If a view contains both a map and reduce function, querying that view will by default return the result
    * of the reduce function. To avoid this behaviour you must call this method.
+   * @return ViewQueryOpts
+   * @attention Chainable.
    */
   public function doNotReduce() {
     $this->options["reduce"] = "false";
@@ -176,6 +210,8 @@ class ViewQueryOpts extends AbstractOpts {
    * @details However, the user should keep in mind that there is a race condition when using this option. It is
    * possible that between reading the view data and fetching the corresponding document that the document has changed.
    * @warning You can call this method only if the view doesn't contain a reduce function.
+   * @return ViewQueryOpts
+   * @attention Chainable.
    */
   public function includeDocs() {
     $this->options["include_docs"] = "true";
@@ -185,6 +221,7 @@ class ViewQueryOpts extends AbstractOpts {
 
   /**
    * @brief Don't get any data, but all meta-data for this View. The number of documents in this View for example.
+   * @attention Chainable.
    */
   public function excludeResults() {
     $this->options["limit"] = 0;
@@ -194,6 +231,8 @@ class ViewQueryOpts extends AbstractOpts {
 
   /**
    * @brief Tells CouchDB to not include end key in the result.
+   * @return ViewQueryOpts
+   * @attention Chainable.
    */
   public function excludeEndKey() {
     $this->options["inclusive_end"] = "false";
@@ -225,6 +264,8 @@ class ViewQueryOpts extends AbstractOpts {
    * faster results. Remember, in case, to use an updater script that calls the views periodically, for example using a
    * cron. You can find the implementation in Ruby or Python in the document
    * [Update views on document save](http://wiki.apache.org/couchdb/Regenerating_views_on_update).
+   * @return ViewQueryOpts
+   * @attention Chainable.
    */
   public function doNotRefreshView() {
     $this->options["stale"] = "ok";
@@ -234,6 +275,8 @@ class ViewQueryOpts extends AbstractOpts {
 
   /**
    * @brief CouchDB will update the view after the query's result is returned.
+   * @return ViewQueryOpts
+   * @attention Chainable.
    */
   public function queryThenRefreshView() {
     $this->options["stale"] = "update_after";
@@ -247,6 +290,8 @@ class ViewQueryOpts extends AbstractOpts {
    * @brief Reverses order of results.
    * @details Note that the descending option is applied before any key filtering, so you may need to swap the values
    * of the start key and end key options to get the expected results.
+   * @return ViewQueryOpts
+   * @attention Chainable.
    */
   public function reverseOrderOfResults() {
     $this->options["descending"] = "true";
@@ -260,7 +305,9 @@ class ViewQueryOpts extends AbstractOpts {
    * inefficient (it scans the index from the start key and then skips N elements, but still needs to read all the index
    * values to do that). For efficient paging you'll need to use start key and limit.
    * Allowed values: positive integers.
-   * @param[in] integer $number The number of rows to skip.
+   * @param[in] int $number The number of rows to skip.
+   * @return ViewQueryOpts
+   * @attention Chainable.
    */
   public function skipDocs($number) {
     if (is_int($number) && $number > 0)
@@ -274,6 +321,8 @@ class ViewQueryOpts extends AbstractOpts {
 
   /**
    * @brief Includes conflict documents.
+   * @return ViewQueryOpts
+   * @attention Chainable.
    */
   public function includeConflicts() {
     $this->options["conflicts"] = "true";
@@ -283,6 +332,8 @@ class ViewQueryOpts extends AbstractOpts {
 
   /**
    * @brief Includes an `update_seq` value indicating which sequence id of the database the view reflects.
+   * @return ViewQueryOpts
+   * @attention Chainable.
    */
   public function includeUpdateSeq() {
     $this->options['update_seq'] = "true";
@@ -292,6 +343,8 @@ class ViewQueryOpts extends AbstractOpts {
 
   /**
    * @brief Includes all the rows, even if a match for a key is not found.
+   * @return ViewQueryOpts
+   * @attention Chainable.
    */
   public function includeMissingKeys() {
     $this->includeMissingKeys = TRUE;
@@ -301,6 +354,8 @@ class ViewQueryOpts extends AbstractOpts {
 
   /**
    * @brief Returns `true` if includeMissingKeys() has been called before.
+   * @return ViewQueryOpts
+   * @attention Chainable.
    */
   public function issetIncludeMissingKeys() {
     return $this->includeMissingKeys;
