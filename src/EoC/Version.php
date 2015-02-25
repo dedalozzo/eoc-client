328291083c54fc215344bb5779730e48aec4de8b
Filippo Fadda <filippo.fadda@gmail.com>
2015-02-25 17:29:52 +0100

    updated MINOR to 4

--- src/EoC/Version.php
+++ src/EoC/Version.php
@@ -19,8 +19,8 @@ namespace EoC;
 class Version {
 
   const MAJOR = '0'; //!< Major release number.
-  const MINOR = '3'; //!< Minor release number.
-  const MAINTENANCE = '2'; //!< Maintenance release number (bug fixes only).
+  const MINOR = '4'; //!< Minor release number.
+  const MAINTENANCE = '0'; //!< Maintenance release number (bug fixes only).
 
 
   /**
