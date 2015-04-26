c0e8113b997b066f7854d5ab100951ff24f91180
Filippo Fadda <filippo.fadda@gmail.com>
2015-04-26 18:50:15 +0200

    version 0.4.2

--- src/EoC/Version.php
+++ src/EoC/Version.php
@@ -20,7 +20,7 @@ class Version {
 
   const MAJOR = '0'; //!< Major release number.
   const MINOR = '4'; //!< Minor release number.
-  const MAINTENANCE = '1'; //!< Maintenance release number (bug fixes only).
+  const MAINTENANCE = '2'; //!< Maintenance release number (bug fixes only).
 
 
   /**
